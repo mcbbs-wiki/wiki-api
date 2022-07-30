318e918616fd04f209297c6a0bbc3d71d43fba99
litwak913 <litwak913@qq.com>
2022-07-30 10:16:23 +0800

    fix:配置文件

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,16 @@
-import { statSync, writeFileSync } from 'fs'
+import { statSync, writeFileSync, readFileSync } from 'fs'
 import consola from 'consola'
 
-interface User{
-    username:string,
-    password:string
-}
 export class Config {
   dbpath:string = './db.db'
-  users:User[] = [{ username: 'user', password: '123456' }]
   port:number = 8080
   constructor (path:string) {
     consola.info(`Reading config ${path}`)
     try {
       statSync(path)
+      const obj = JSON.parse(readFileSync(path).toString())
+      this.dbpath = obj.dbpath
+      this.port = obj.port
     } catch {
       writeFileSync(path, JSON.stringify(this))
       consola.info(`Creating new config ${path}`)
--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,17 @@ import img from './routers/img.js'
 import user from './routers/user.js'
 import admin from './routers/admin.js'
 import { open } from 'sqlite'
+import { Server } from 'http'
 
 const config = new Config('./config.json')
 consola.info(`Reading database ${config.dbpath}`)
 const db = await open({
-  filename: './db.db',
+  filename: config.dbpath,
   driver: sqlite3.Database
 })
 const app = express()
 app.set('view engine', 'ejs')
-//app.use('/static', express.static('./public'))
+// app.use('/static', express.static('./public'))
 app.use(await img(db))
 app.use(user)
 app.use(admin)
@@ -26,12 +27,14 @@ app.use(function (err:any, req:any, res:any, next:any) {
   consola.error(err)
   res.status(500).send('')
 })
-const server = app.listen(18080, () => { consola.info('Server started at http://0.0.0.0:18080') })
-
-process.on('SIGINT', () => {
+const server = app.listen(config.port, () => { consola.info(`Server started at http://0.0.0.0:${config.port}`) })
+function shutdown (server:Server) {
   server.close(async () => {
     consola.info('Shuting down')
     await db.close()
     consola.info('Closing database')
   })
-})
+}
+
+process.on('SIGINT', () => shutdown(server))
+process.on('SIGTERM', () => shutdown(server))
