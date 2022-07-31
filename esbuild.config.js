acd1d9036b5cf4c171c419e87bf599a97fa689ed
litwak913 <litwak913@qq.com>
2022-07-31 22:14:04 +0800

    chore:添加nodemon和cross-env

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,7 +2,6 @@ import { build } from 'esbuild'
 import { nodeExternalsPlugin } from 'esbuild-node-externals'
 import { tgz } from 'compressing'
 import { createWriteStream } from 'fs'
-const stream = new tgz.Stream()
 console.info('Bundling...')
 build({
   entryPoints: ['./src/index.ts'],
@@ -10,12 +9,15 @@ build({
   outfile: 'dist/bundle.js',
   platform: 'node',
   format: 'esm',
-  minify: true,
+  minify: process.env.NODE_ENV === 'production',
   plugins: [nodeExternalsPlugin()]
 })
-console.info('Compressing...')
-stream.addEntry('./package.json')
-stream.addEntry('./dist/bundle.js')
-const destStream = createWriteStream('dist/bundle.tar.gz')
-stream.pipe(destStream)
+if (process.env.NODE_ENV === 'production') {
+  const stream = new tgz.Stream()
+  console.info('Compressing...')
+  stream.addEntry('./package.json')
+  stream.addEntry('./dist/bundle.js')
+  const destStream = createWriteStream('dist/bundle.tar.gz')
+  stream.pipe(destStream)
+}
 console.info('Ready!')
