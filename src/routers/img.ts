abcd1cddb6001b37a91bb29c17dd575456b7ce16
litwak913 <litwak913@qq.com>
2022-07-31 16:45:33 +0800

    feat:大小图片选择

--- src/routers/img.ts
+++ src/routers/img.ts
@@ -1,60 +1,76 @@
-import express, { Router, Request, Response } from 'express'
+import express, { Router } from 'express'
 import isNumber from 'is-number'
 import randomInteger from 'random-int'
-import consola from 'consola'
+// import consola from 'consola'
 import { Pool, RowDataPacket } from 'mysql2/promise'
-// import { inspect } from 'util'
 
 let db:Pool
 const routerImg = express.Router()
-const SQL = 'select * from imgs where id=?;'
+const SQL = 'select * from imgs;'
+const SQL_SMALL = 'select * from imgs where small=1;'
+const SQL_NORMAL = 'select * from imgs where small=0;'
+const SQL_ID = 'select * from imgs where id=?'
 const USAGE_SQL = 'INSERT INTO usages VALUES (NULL,"img",?,?,?,?,?,?);'
-let imgnum:number
+// let imgnum:number
 
-function usage (random:boolean, pid:number, status:number, ip:string, ua:string, extra:string | null) {
-  db.execute(USAGE_SQL, [random ? 0 : pid, status, ip, ua, new Date(), extra])
+function usage (pid:number, status:number, ip:string, ua:string, extra:string | null) {
+  db.execute(USAGE_SQL, [pid, status, ip, ua, new Date(), extra])
 }
 routerImg.get('/imgs', async (req, res) => {
-  // console.log(db.getMaxListeners())
-  // ssb.getMaxListeners()
-  queryImg(randomInteger(1, imgnum).toString(), req, res, true)
-})
-routerImg.get('/imgs/:id', async (req, res) => {
-  queryImg(req.params.id, req, res, false)
-})
-async function queryImg (id:string, req:Request, res:Response, random:boolean) {
   res.header('Access-Control-Allow-Origin', '*')
-  const pid = parseInt(id)
+  // const pid = parseInt(req.params.id)
   const preua = req.header('User-Agent')
   const ua = preua !== undefined ? preua : ''
   const typeJson = req.query.type === 'json'
-  if (isNumber(id)) {
-    // consola.log(id)
-    if (typeJson) {
-      const [query] = await db.query(SQL, [pid])
-      const img = query as RowDataPacket[]
-      if (img.length !== 0) {
-        usage(random, pid, 200, req.ip, ua, 'json')
-        res.header('Cache-Control', 'no-store')
+  let query:unknown
+  if (req.query.size && req.query.size === 'small') {
+    [query] = await db.query(SQL_SMALL)
+  } else if (req.query.size && req.query.size === 'normal') {
+    [query] = await db.query(SQL_NORMAL)
+  } else {
+    [query] = await db.query(SQL)
+  }
+  const img = query as RowDataPacket[]
+  if (typeJson) {
+    usage(0, 200, req.ip, ua, 'json')
+    res.send(img[randomInteger(1, img.length) - 1])
+  } else {
+    usage(0, 200, req.ip, ua, null)
+    res.redirect(img[randomInteger(1, img.length) - 1].path)
+  }
+})
+routerImg.get('/imgs/:id', async (req, res) => {
+  // queryImg(req.params.id, req, res, false)
+  res.header('Access-Control-Allow-Origin', '*')
+  const pid = parseInt(req.params.id)
+  const preua = req.header('User-Agent')
+  const ua = preua !== undefined ? preua : ''
+  const typeJson = req.query.type === 'json'
+  if (isNumber(pid)) {
+    const [query] = await db.query(SQL_ID, [pid])
+    const img = query as RowDataPacket[]
+    if (img[0] !== undefined) {
+      if (typeJson) {
+        usage(pid, 200, req.ip, ua, 'json')
         res.send(img[0])
       } else {
-        usage(random, pid, 404, req.ip, ua, 'json')
-        res.status(404).sendFile('/www/wwwroot/mcbbs.wiki/404.html')
+        usage(pid, 200, req.ip, ua, null)
+        res.redirect(img[0].path)
       }
     } else {
-      usage(random, pid, 200, req.ip, ua, null)
-      res.redirect(`/images/memes/${pid}.webp`)
+      usage(pid, 404, req.ip, ua, null)
+      res.status(404).sendFile('/www/wwwroot/mcbbs.wiki/404.html')
     }
   } else {
-    usage(random, pid, 400, req.ip, ua, null)
+    usage(pid, 400, req.ip, ua, null)
     res.status(400).sendFile('/www/wwwroot/mcbbs.wiki/400.html')
   }
-}
+})
 export default async function (conn:Pool):Promise<Router> {
   db = conn
-  const [res] = await db.query('select count(*) as count from imgs;')
+  // const [res] = await db.query('select count(*) as count from imgs;')
   // imgnum = (await db.query('select count(*) as count from imgs;')).count
-  imgnum = (res as RowDataPacket[])[0].count
-  consola.info(`Reading ${imgnum} images.`)
+  // imgnum = (res as RowDataPacket[])[0].count
+  // consola.info(`Reading ${imgnum} images.`)
   return routerImg
 }
